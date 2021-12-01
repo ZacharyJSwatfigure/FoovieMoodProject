6bab714ec9e8489d7fb5155577236b3c823f3cfb
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2021-11-30 21:46:45 -0700

    We did a shit ton XD

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 
 let MovieApi = 'db3a630e';
 let searchQ;
+let countryArr = []
+let foodType = ""
 
 function getSearchQ(SearchMovie) {
     searchQ= 't';
@@ -14,9 +16,16 @@ function Search(SearchMovie){
         })
         .then(result=> {
             init(result);
+        
         })
 
 }
+
+
+
+
+
+
 
 function init(result) {
     console.log (result);
@@ -25,19 +34,79 @@ function init(result) {
     let moviePlot= document.getElementById('movieDescription');
     let mPoster = document.getElementById('Poster');
 
+    let countryArr = result.Country.split(",").map(function (value) {
+        return value.trim();
+    });
+    console.log (countryArr);
+
 
     //let movPoster = new Image(300,450);
+
+
+    
+    switch (countryArr[1]) {
+        case 'United States':
+        case 'Canada':
+            foodType = "American";
+            console.log("American")
+            break;
+        case 'Poland':
+        case 'United Kingdom':    
+        case 'Germany':
+        case 'Ukraine':
+            console.log("Europe Dish");
+            foodType = "Central Europe";
+            break;
+        case 'France':
+        case 'Italy':
+            foodType = "Italian";
+            console.log("Who wants Pasta");
+            break;
+        case 'North Korea':
+        case 'South Korea':
+            console.log();
+            foodType = "Asian";
+            break;
+        case "China":
+            foodType = "Chinese";
+            break;
+        case "Japan":
+            foodType = "Japanese";
+                default:
+            break;
+    }
+        fetch(`https://api.edamam.com/api/recipes/v2?app_id=fea86a91&app_key=2b9e76ada8e917607dc4fd6bd442e887&type=public&cuisineType=${foodType}`)
+            .then( response => {
+                return response.json();
+            })
+            .then( response => {
+                food(response);
+            })
+            function food(response) {
+                console.log(response);
+                for (let i = 0; i < 3; i++) {
+                    myRandomNum = Math.floor(Math.random() * 500)
+                    console.log("myRandomNum")
+                    response.keys[myRandomNum];
+                    console.log(`response.keys[myRandomNum]`)
+
+                }
+            }
+        
+
 
 
     movieT.innerText=result.Title;
     countryOrigin.innerText= result.Country;
     moviePlot.innerText= result.Plot;
-
     posterURL= result.Poster;
     mPoster.src= `${posterURL}`;
     //document.getElementById('moviePoster').append(movPoster);
 
-}
+};
+
+
+
 
 
     document.getElementById('searchBtn').addEventListener('click',()=>
--- readme.md
+++
@@ -1,2 +0,0 @@
-The Foovie Master Chefs!
-Got a favorite movie? -now get served with a cuisine based upon it.
